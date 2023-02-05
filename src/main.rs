a47449fa6efcd3bfd60d8e431e676b5c847fa679
Thomas Kapocsi <thomaskapocsi@Thomass-MacBook-Pro.local>
2023-02-05 16:14:22 -0700

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
 mod database;
-use database::data::{self, AuthenticatedUser};
+use database::data;
 use serde::{Deserialize, Serialize};
 use serde_yaml;
 use std::{fs, time::Duration};
 
+use actix_web_lab::web::spa;
 use futures_util::StreamExt as _;
 use std::future::Future;
 
 use actix_web::{
-    cookie::Cookie, get, post, web, App, FromRequest, HttpResponse, HttpServer, Result,
+    cookie::Cookie,
+    get, post,
+    web::{self, scope},
+    App, FromRequest, HttpMessage, HttpResponse, HttpServer, Result,
 };
 
-#[post("/api/user")]
+#[post("/user")]
 async fn get_user(mut payload: web::Payload) -> Result<HttpResponse> {
     let user_id = {
         let mut bytes = web::BytesMut::new();
@@ -22,13 +26,17 @@ async fn get_user(mut payload: web::Payload) -> Result<HttpResponse> {
     }
     .map_err(|_| actix_web::error::ErrorBadRequest("Could not parse request"))?;
 
+    println!("Fetching User:{}", user_id);
+
+    std::thread::sleep_ms(1000);
+
     Ok(HttpResponse::Found().body(
         fs::read_to_string(format!("database/users/{}.json", user_id))
             .map_err(|_| actix_web::error::ErrorNotFound("User not found"))?,
     ))
 }
 
-#[post("/api/newuser/{name}")]
+#[post("/newuser/{name}")]
 async fn generate_user(path: web::Path<(String)>) -> Result<HttpResponse> {
     println!("{path}");
     let username = path.into_inner();
@@ -40,75 +48,13 @@ async fn generate_user(path: web::Path<(String)>) -> Result<HttpResponse> {
     Ok(response)
 }
 
-#[derive(Serialize, Deserialize)]
-struct UserCredentials {
-    username: String,
-    password: String,
-}
-
-#[post("/api/auth/newuser")]
-async fn new_auth_user(mut payload: web::Payload) -> Result<HttpResponse> {
-    let new_user: UserCredentials = serde_json::de::from_str({
-        let mut bytes = web::BytesMut::new();
-        while let Some(item) = payload.next().await {
-            bytes.extend_from_slice(&item?);
-        }
-        String::from_utf8(bytes.to_vec())
-            .map_err(|_| actix_web::error::ErrorBadRequest("Could not parse request"))?
-            .as_str()
-    })?;
-    data::AuthenticatedUser::new(new_user.username, new_user.password).push_to_data_base();
-
-    Ok(HttpResponse::Ok().finish())
-}
-
-:#[post("/api/auth/login")]
-async fn authenticate_user(mut payload: web::Payload) -> Result<HttpResponse> {
-    let new_user: UserCredentials = serde_json::de::from_str({
-        let mut bytes = web::BytesMut::new();
-        while let Some(item) = payload.next().await {
-            bytes.extend_from_slice(&item?);
-        }
-        String::from_utf8(bytes.to_vec())
-            .map_err(|_| actix_web::error::ErrorBadRequest("Could not parse request"))?
-            .as_str()
-    })?;
-
-    let mut user: data::AuthenticatedUser =
-        data::AuthenticatedUser::read_from_data_base(new_user.username, new_user.password)
-            .map_err(|_| actix_web::error::ErrorNotFound("Username or password incorrect"))?;
-
-    user.attach_token();
-
-
-    println!("{:?}", user.auth_token);
-    let cookieString = serde_json::ser::to_string(&user.auth_token).unwrap();
-    user.push_to_data_base();
-
-    Ok(HttpResponse::Found()
-        .body(cookieString)
-        .add_cookie(Cookie::new("token", cookieStr)
-}
-
-#[get("/printer")]
-async fn printer(mut payload: web::Payload) -> Result<HttpResponse> {
-    let mut bytes = web::BytesMut::new();
-    while let Some(item) = payload.next().await {
-        bytes.extend_from_slice(&item?);
-    }
-    println!("{:?}", bytes);
-
-    Ok(HttpResponse::Ok().body(bytes))
-}
-
 #[derive(Serialize, Deserialize, Debug)]
 struct InspectPost {
     user_uuid: String,
     inspection_to_post: data::Inspection,
-    auth_token: data::AuthToken,
 }
 
-#[post("/api/post-inspection")]
+#[post("/post-inspection")]
 async fn add_inspection_to_user(mut payload: web::Payload) -> Result<HttpResponse> {
     let request: InspectPost = serde_json::de::from_str({
         let mut bytes = web::BytesMut::new();
@@ -120,12 +66,6 @@ async fn add_inspection_to_user(mut payload: web::Payload) -> Result<HttpRespons
             .as_str()
     })?;
 
-    // Check if user is authorized, we early return if not we continue
-    match request.auth_token.is_valid() {
-        Ok(true) => Ok(true),
-        Err(_) | Ok(false) => Err(actix_web::error::ErrorUnauthorized("Not Authorized")),
-    }?;
-
     // Load the user and append the inspection
     let mut inspectee = data::User::read_from_database(request.user_uuid)?;
     inspectee.push_inspection(request.inspection_to_post);
@@ -134,57 +74,37 @@ async fn add_inspection_to_user(mut payload: web::Payload) -> Result<HttpRespons
     Ok(actix_web::HttpResponse::Ok().finish())
 }
 
-#[get("/api/inspections.json")]
+#[get("/inspections.json")]
 async fn return_inspections() -> Result<HttpResponse> {
     let inspections = data::load_inspection_list()
         .map_err(|_| actix_web::error::ErrorInternalServerError("Internal Error Occured"))?;
+
+    println!("Served inspection list");
+
     Ok(HttpResponse::Found()
         .body(serde_json::ser::to_string(&inspections).expect("This should always work")))
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let test = InspectPost {
-        user_uuid: "UUID".to_owned(),
-        inspection_to_post: data::Inspection {
-            name: "Blues".to_owned(),
-            criteria: vec![
-                data::Criteria::Graded(data::CriteriaGraded {
-                    category_name: "Category Name".to_string(),
-                    descriptions: vec!["Description 1".to_string()],
-                    state: Some(1),
-                }),
-                data::Criteria::PassFail(data::CriteriaPassFail {
-                    description: "Pass Fail".to_string(),
-                    state: Some(false),
-                    category_name: "Category Name".to_string(),
-                }),
-                data::Criteria::Comment(Some("A comment".to_string())),
-            ],
-            date: Some(0),
-        },
-        auth_token: {
-            let mut user = AuthenticatedUser::new("Thomas".to_string(), "1234".to_string());
-            user.attach_token();
-            user.push_to_data_base();
-            let user =
-                AuthenticatedUser::read_from_data_base("Thomas".to_string(), "1234".to_string())
-                    .unwrap();
-            user.auth_token.unwrap()
-        },
-    };
-
-    println!("{}", serde_json::to_string(&test).unwrap());
-
+    use actix_files as fs;
     HttpServer::new(|| {
         App::new()
-            .service(get_user)
-            .service(generate_user)
-            .service(new_auth_user)
-            .service(authenticate_user)
-            .service(add_inspection_to_user)
-            .service(return_inspections)
-            .service(printer)
+            .service(
+                scope("/api")
+                    .service(get_user)
+                    .service(generate_user)
+                    .service(return_inspections)
+                    .service(add_inspection_to_user)
+                    .service(return_inspections),
+            )
+            .service(
+                spa()
+                    .index_file("../yew-front-end/dist/index.html")
+                    .static_resources_mount("")
+                    .static_resources_location("../yew-front-end/dist")
+                    .finish(),
+            )
     })
     .bind(("127.0.0.1", 8000))?
     .run()
